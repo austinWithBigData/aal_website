f40aacd241ca5042294f7979a29b99f09ce43898
AustinS <austin.sun@torqata.com>
2022-04-24 15:27:49 -0400

    initial upload.

---
+++ oauth/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from .models import OAuthConfig
+
+
+# Create your tests here.
+class OAuthConfigTest(TestCase):
+    def config_save_test(self):
+        c = OAuthConfig()
+        c.type = 'weibo'
+        c.appkey = 'appkey'
+        c.appsecret = 'appsecret'
+        c.save()
